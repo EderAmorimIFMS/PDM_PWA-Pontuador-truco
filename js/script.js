e4258c74514553c24707dd23040ea746b70c1106
EderAmorimIFMS <eder.amorim@estudante.ifms.edu.br>
2023-08-21 11:20:17 -0400

    arrumando esta 80%, falta correções no evento de pontuar e truco

--- js/script.js
+++ js/script.js
@@ -1,57 +1,54 @@
-let equipe_1, equipe_2;
-equipe_1 = document.getElementById("equipe_1");
-equipe_2 = document.getElementById("equipe_2");
+let pontoEquipe1, pontoEquipe2, valortruco;
+pontoEquipe1 = 0;
+pontoEquipe2 = 0;
+valortruco = 1;
 
-let pontos_equipe1, pontos_equipe2;
-pontos_equipe1 = parseFloat(document.getElementById("ponto_1"));
-pontos_equipe2 = parseFloat(document.getElementById("ponto_2"));
-
-let valorDoPonto = document.getElementById("valorPonto");
-valorPonto = 1;
-
-function Logar(){
+/*lfunction Logar(){
   let input1 = document.getElementById("equipe1").value
   let input2 = document.getElementById("equipe2").value
   
-  equipe_1.innerHTML = equipe1
-  equipe_2.innerHTML = equipe2
+  equipe_1.innerHTML = input1
+  equipe_2.innerHTML = input2
   pontos_equipe1.innerHTML = 0
   pontos_equipe2.innerHTML = 0
-}
+}*/
 
-function Pontuar_equipe1(){
+function Ponto_equipe1(){
   if(pontos_equipe1 == 12){
-    document.getElementById("vencedor1").innerHTML = "Ganhoooooouuuu";
+     alert("Equipe/Jogador 2 Ganhoooooouuuu");
   } else{
-    pontos_equipe1.innerHTML += valorPonto;
+    pontoEquipe1 += valortruco;
+    document.getElementById("ponto_1").innerHTML = pontoEquipe1;
+    valortruco = 1;
   }
 }
 
-function Pontuar_equipe2(){
+function Ponto_equipe2(){
   if(pontos_equipe2 == 12){
-    document.getElementById("vencedor2").innerHTML = "Ganhoooooouuuu"
+      alert("Equipe/Jogador 2 Ganhoooooouuuu")
   } else {
-    pontos_equipe2.innerHTML += valorPonto;
+    pontoEquipe2 += valortruco;
+    document.getElementById("ponto_2").innerHTML = pontoEquipe2;
+    valortruco = 1;
   }
 }
 
 function Retirar_equipe1(){
-  pontos_equipe1.innerHTML -= 1;
+  pontoEquipe1 -= 1;
+  document.getElementById("ponto_1").innerHTML = pontoEquipe1;
 }
 
 function Retirar_equipe2(){
-  pontos_equipe2.innerHTML -= 1;
+  pontoEquipe2 -= 1;
+    document.getElementById("ponto_2").innerHTML = pontoEquipe2;
 }
 
-let onpress_truco = false
-
 function Trucar(){
-  onpress_truco = true
-  valorDoPonto = 0
-  
-  if(onpress_truco == true){
-    onpress_truco = false
-    valorDoPonto += 3
-    valorDoPonto.innerHTML 
+  if(valorDoPonto == 1){
+    valortruco = 3;
+    document.getElementById("p_truco").innerHTML = valortruco;
+  } else {
+    valortruco += 3;
+    document.getElementById("p_truco").innerHTML = valortruco;
   }
 }
